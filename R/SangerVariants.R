f7e435a3c4f1165fb65d4bd52eafc1c9873e8d79
JanEngelstaedter <j.engelstaedter@uq.edu.au>
2023-08-05 12:01:33 +1000

    first complete version

---
+++ R/SangerVariants.R
@@ -0,0 +1,152 @@
+# Functions to call variants from Sanger sequencing data
+
+#' Title
+#'
+#' @param sampleKey_file File name to a table specifying the samples.
+#' This table must have the following columns:
+#' * SampleID: a unique ID for each bacterial sample
+#' * Species
+#' * Strain
+#' * Gene
+#' * Primer: the name of the primer used for the sequencing
+#' * FileName: the name of the fasta file (without file extension)
+#' * Success: whether or not the sequencing was successful (TRUE or FALSE)
+#' * Reference: the name of the corresponding reference sequence
+#' @param fasta_path The path where the fasta files are located.
+#' @param fasta_extension File extension of the fasta files, usually either ".fa" (default) or ".fasta".
+#' @param reference_files The path and file name(s) of the fasta file(s) with the reference sequences
+#' @param hardTrim A vector specifying how many nucleotides on each side of a sequence should be hard-trimmed.
+#'
+#' @return A list containing two tables: "summary" list the number of variants of different types detected for each sample,
+#' whereas "details" provides detailed information for each variant.
+#' @export
+#'
+callSangerVariants_fasta <- function(sampleKey_file,
+                                     fasta_path,
+                                     fasta_extension = ".fa",
+                                     reference_files,
+                                     hardTrim = c(0, 0)) {
+
+  # read in, check and sort sampleKey:
+  sampleKey <- readr::read_csv(sampleKey_file)
+  if (!all(names(sampleKey)[1:8] == c("SampleID",
+                                      "Species",
+                                      "Strain",
+                                      "Gene",
+                                      "Primer",
+                                      "FileName",
+                                      "Success",
+                                      "Reference"))) {
+    stop("Columns in sampleKey file don't confirm to what is expected - refer to documentation.")
+  }
+  sampleKey <- sampleKey |>
+    dplyr::arrange(Gene, SampleID)
+
+  # read in and prepare fasta files for samples:
+  fastaFileNames <- paste0(fasta_path,
+                           sampleKey$FileName[sampleKey$Success],
+                           fasta_extension)
+  sampleSeqs <- Biostrings::readDNAStringSet(fastaFileNames) |>
+    DECIPHER::RemoveGaps() |>
+    XVector::subseq(hardTrim[1] + 1, -hardTrim[2] - 1) # trim base pairs on both sides
+
+  # read in and prepare reference sequences:
+  referenceSeqs <- Biostrings::readDNAStringSet(reference_files)
+
+  # calculate coordinates - hard-coded for now to only work for E. coli MG1655 ref:
+  genes <- unique(sampleKey$Gene)
+  coordinates <- vector(mode = "list", length = length(genes))
+  names(coordinates) <- genes
+  for(i in 1:length(genes)) {
+    refs <- referenceSeqs[unique(sampleKey$Reference[sampleKey$Gene == sampleKey$Gene[i]])]
+    ref_Ecoli <- paste0(genes[i], "_Escherichia_coli_MG1655")
+    coordinates[[i]] <- getAllCoordinates(refs, ref_Ecoli)
+  }
+
+  # prepare variants data frames:
+  variantsSummary <- sampleKey |>
+    dplyr::mutate(ContigLength = NA,
+                  MapStart = NA,
+                  MapEnd = NA,
+                  nVariants = NA,
+                  nMismatches = NA,
+                  nInsertions = NA,
+                  nDeletions = NA)
+
+  variantsDetails <- data.frame(SampleID = rep(NA, 1000),  # make long data frame for more efficiency
+                         Species = NA,
+                         Strain = NA,
+                         Gene = NA,
+                         Primer = NA,
+                         FileName = NA,
+                         Reference = NA,
+                         ContigLength = NA,
+                         MapStart = NA,
+                         MapEnd = NA,
+                         VariantType = NA,
+                         Nt_pos = NA,
+                         Nt_pos_Ecoli = NA,
+                         Nt_original = NA,
+                         Nt_mutation = NA,
+                         Nt_OtoM = NA,
+                         Nt_mut_name = NA,
+                         Nt_mut_name_Ecoli = NA,
+                         Codon_original = NA,
+                         Codon_mutation = NA,
+                         Codon_OtoM = NA,
+                         AA_pos = NA,
+                         AA_pos_Ecoli = NA,
+                         AA_original = NA,
+                         AA_mutation = NA,
+                         AA_OtoM = NA,
+                         AA_mut_name = NA,
+                         AA_mut_name_Ecoli = NA)
+
+  j <- 1 # tracker for row in variantsDetails table
+  for(i in (1:nrow(sampleKey))[sampleKey$Success]) {
+    svMisc::progress(i, progress.bar = TRUE, max.value = nrow(sampleKey))
+
+    # mapping of contig to reference:
+    contig <- sampleSeqs[[sampleKey$FileName[i]]]
+    ref <- referenceSeqs[[sampleKey$Reference[i]]]
+    alig <- Biostrings::pairwiseAlignment(contig, ref, type = "local")
+    mismatches <- Biostrings::mismatchTable(alig)
+    indels <- Biostrings::indel(alig)
+
+    # fill in variantsSummary table:
+    variantsSummary$ContigLength[i] <- length(contig)
+    variantsSummary$MapStart[i] <- alig@subject@range@start
+    variantsSummary$MapEnd[i] <- alig@subject@range@start + alig@subject@range@width - 1
+    variantsSummary$nVariants[i] <- nrow(mismatches) +
+      length(indels@insertion[[1]]) + length(indels@deletion[[1]])
+    variantsSummary$nMismatches[i] <- nrow(mismatches)
+    variantsSummary$nInsertions[i] <- length(indels@insertion[[1]])
+    variantsSummary$nDeletions[i] <- length(indels@deletion[[1]])
+
+    # fill in variantsDetails table:
+    if (variantsSummary$nMismatches[i] > 0) { # any mismatches?
+
+      for(k in 1:nrow(mismatches)) {
+        variantsDetails[j, 1:10] <- variantsSummary[i, 1:10] # same first columns
+        variantsDetails$VariantType[j] <- "substitution"
+        variantsDetails$Nt_pos[j] <- mismatches$SubjectStart[k]
+        variantsDetails$Nt_original[j] <- mismatches$SubjectSubstring[k]
+        variantsDetails$Nt_mutation[j] <- mismatches$PatternSubstring[k]
+        variantsDetails <- fillMutationsTableRow_DNA(variantsDetails,
+                                                     j,
+                                                     sampleKey$Reference[i],
+                                                     referenceSeqs,
+                                                     coordinates[[sampleKey$Gene[i]]])
+        variantsDetails <- fillMutationsTableRow_Protein(variantsDetails,
+                                                         j,
+                                                         sampleKey$Reference[i],
+                                                         referenceSeqs,
+                                                         coordinates[[sampleKey$Gene[i]]])
+        j <- j + 1
+      }
+    }
+  }
+  variantsDetails <- variantsDetails[1:(j - 1),]
+  return(list(summary = variantsSummary,
+              details = variantsDetails))
+}
---
+++ R/coordinates.R
@@ -0,0 +1,131 @@
+# Functions relating to coordinates, i.e. tables relating positions in one focal sequence
+# to positions in a homologous reference sequence (usually in E. coli).
+
+
+#' Calculate a table of coordinates
+#'
+#' This function takes two DNA sequences, aligns the translated amino acid sequences,
+#' and calculates a table of corresponding positions in this alignment ("coordinates").
+#'
+#' @param dnaFocal Focal DNA sequence.
+#' @param dnaRef Reference DNA sequence, e.g. Escherichia coli.
+#'
+#' @return A table with two columns: the position in the focal sequence (posFocal),
+#' and the position in the reference sequence (posRef).
+#'
+#' @export
+#'
+getCoordinates <- function(dnaFocal, dnaRef) {
+  AAFocal <- Biostrings::translate(dnaFocal,
+                                   genetic.code = Biostrings::getGeneticCode("Bacterial",
+                                                                             full.search = TRUE))
+  AARef <- Biostrings::translate(dnaRef,
+                                 genetic.code = Biostrings::getGeneticCode("Bacterial",
+                                                                           full.search = TRUE))
+  alig <- Biostrings::pairwiseAlignment(AAFocal, AARef)
+
+  # extract the aligned sequences from the alignment
+  aligFocal <- alig |>
+    Biostrings::alignedPattern() |>
+    toString() |>
+    stringr::str_replace_all("[a-zA-Z*]", "NNN") |>
+    stringr::str_replace_all("[-]", "---") |>
+    Biostrings::DNAString()
+
+  aligRef <- alig |>
+    Biostrings::alignedSubject() |>
+    toString() |>
+    stringr::str_replace_all("[a-zA-Z*]", "NNN") |>
+    stringr::str_replace_all("[-]", "---") |>
+    Biostrings::DNAString()
+
+  posFocal <- 0L
+  posRef <- 0L
+  coordinates <- data.frame(posFocal = rep(NA_integer_, length(aligFocal)),
+                            posRef   = NA_integer_)
+  for (i in 1:length(aligFocal)) {
+    letterFocal <- Biostrings::extractAt(aligFocal, IRanges::IRanges(i)) |> as.character()
+    letterRef <- Biostrings::extractAt(aligRef, IRanges::IRanges(i)) |> as.character()
+    if (letterFocal != "-")
+      posFocal <- posFocal + 1L
+    if (letterRef != "-")
+      posRef <- posRef + 1L
+    coordinates$posFocal[i] <- posFocal
+    coordinates$posRef[i] <- posRef
+  }
+  return(coordinates)
+}
+
+#' Obtain tables of coordinates.
+#'
+#' Takes a list of homologous sequences of which one is designated a reference,
+#' aligns them all to the reference (at amino-acid level), and calculates for each sequence
+#' a table of corresponding coordinates.
+#'
+#' @param seqs A DNAStringSet object containing a set of homologous sequences.
+#' @param refSeq_ID The name of the sequence designated as the reference.
+#'
+#' @return A list of tables of coordinates. Each of these tables has two columns:
+#' the position in the focal sequence (posFocal), and the position in the reference sequence (posRef).
+#' @export
+#'
+getAllCoordinates <- function(seqs, refSeq_ID) {
+  coordinates <- vector(mode = "list", length = length(seqs))
+  names(coordinates) <- names(seqs)
+  for(i in 1:length(seqs)) {
+    cat(paste0("Determining coordinates for ", names(seqs)[i], ".\n"))
+    coordinates[[i]] <- getCoordinates(seqs[[i]], seqs[[refSeq_ID]])
+  }
+  return(coordinates)
+}
+
+#' Translates between positions in two sequences.
+#'
+#' Using coordinates calculated using the `getCoordinates` or `getAllCoordinates` function,
+#' this function translates between a position in a focal sequence to a position in
+#' the reference sequence, or vice versa.
+#'
+#' @param pos Position within the sequence.
+#' @param coordinates Table of coordinates.
+#' @param direction Either "FocalToRef" (the default) or "RefToFocal".
+#' @param AAinput When `FALSE` (the default), the `pos` argument will be assumed to be a
+#' nucleotide position. Otherwise, `pos` will be treated as an amino acid position.
+#' @param AAoutput When `FALSE` (the default), the returned translated position will be a nucleotide
+#' position, otherwise an amino acid position.
+#'
+#' @return The translated position.
+#' @export
+#'
+translateCoordinate <- function(pos,
+                                coordinates,
+                                direction = "FocalToRef",
+                                AAinput = FALSE,
+                                AAoutput = FALSE) {
+  if (AAinput) {
+    pos <- pos * 3L - 2L
+  }
+  if(length(pos) > 1) {
+    pos <- pos[1]
+    warning("More than one position in pos vector, only the first one will be used.")
+  }
+  if (direction == "FocalToRef") {
+    coordinates$posRef[duplicated(coordinates$posRef) |
+                           (coordinates$posRef == 0)] <- NA
+    trans <- coordinates |>
+      dplyr::filter(posFocal == pos) |>
+      dplyr::pull(posRef) |>
+      dplyr::first()
+  } else if (direction == "RefToFocal") {
+    coordinates$posFocal[duplicated(coordinates$posFocal) |
+                           (coordinates$posFocal == 0L)] <- NA
+    trans <- coordinates |>
+      dplyr::filter(posRef == pos) |>
+      dplyr::pull(posFocal) |>
+      dplyr::first()
+  } else {
+    stop("Argument direction must be either FocalToRef or RefToFocal")
+  }
+  if (AAoutput == TRUE)
+    trans <- ((trans - 1L) %/% 3L) + 1L
+  return(trans)
+}
